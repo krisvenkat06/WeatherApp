2d8b46ba36feae612fa39cea5ea7518f157a7c7a
Venkatesh Krishna Moorthi <krisvenkat06@gmail.com>
2018-10-09 09:26:59 -0500

    adding end to end test scripts

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,29 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
-describe('weather-appp App', () => {
+describe('Weather App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display weather in Header', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Weather');
   });
+
+  it('should display weather info for 4 cities',()=>{
+    expect(page.getCitiesCount().count()).toBe(4);
+  });
+
+  it('should display 10days forecast for Dallas', ()=>{
+    page.clickForecast();
+    expect(page.getDallasForecast().getText()).toEqual('Forecast for Dallas');
+  });
+
+  it('should navigate to Current Weather Page', ()=> {
+    page.goToCurrentWeather();
+    expect(page.getCitiesCount().count()).toBe(4);
+  });  
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -8,4 +8,36 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getCitiesCount() {
+    return element.all(by.css('app-weather'));
+  }
+
+  clickForecast(){
+    let forecast = element(by.partialLinkText('10 Days Forecast'));
+    forecast.click();
+  }
+
+  getDallasForecast(){
+    return element(by.cssContainingText('#forecastCity', 'Forecast for Dallas'));
+  }
+
+  goToCurrentWeather(){
+    let currentWeather = element(by.partialLinkText('Current Weather'));
+    currentWeather.click();
+  }
+
+  isCityPresent(city){
+    return element(by.cssContainingText('.loc-container.txt-cntr>span',city)).isDisplayed();
+  }
+
+  viewForecast(){
+    let cityForecast = element(by.partialLinkText('View Forecast'));
+    cityForecast.click();
+  }
+
+  getSelectedCityForecast(){
+    return element(by.cssContainingText('#forecastCity', 'Forecast for Atlanta'));
+
+  }
 }
